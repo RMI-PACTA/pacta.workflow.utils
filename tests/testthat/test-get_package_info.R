f2c816ffbcb4841a9d4b3c07cd8d2cbe63108c33
Alex Axthelm <aaxthelm@RMI.org>
2024-03-25 10:47:33 +0100

    re-enable tests

--- tests/testthat/test-get_package_info.R
+++ tests/testthat/test-get_package_info.R
@@ -125,70 +125,70 @@ test_that("get_package_info outputs expected value for simple named list", {
 empty_named_list <- list()
 names(empty_named_list) <- character(0L)
 
-# test_that("get_package_info with empty character args returns empty named lists", { #nolint: line_length_linter
-#   expect_identical(
-#     object = get_package_info(list(
-#       base = character(),
-#       attached = character(),
-#       loaded = character()
-#     )),
-#     expected = list(
-#       base = empty_named_list,
-#       attached = empty_named_list,
-#       loaded = empty_named_list
-#     )
-#   )
-# })
+test_that("get_package_info with empty character args returns empty named lists", { #nolint: line_length_linter
+  expect_identical(
+    object = get_package_info(list(
+      base = character(),
+      attached = character(),
+      loaded = character()
+    )),
+    expected = list(
+      base = empty_named_list,
+      attached = empty_named_list,
+      loaded = empty_named_list
+    )
+  )
+})
 
-# test_that("get_package_info with empty args returns empty lists", {
-#   expect_identical(
-#     object = get_package_info(list(
-#       base = c(), # nolint: unnecessary_concatenation_linter
-#       attached = c(), # nolint: unnecessary_concatenation_linter
-#       loaded = c() # nolint: unnecessary_concatenation_linter
-#     )),
-#     expected = list(
-#       base = list(),
-#       attached = list(),
-#       loaded = list()
-#     )
-#   )
-# })
+test_that("get_package_info with empty args returns empty lists", {
+  expect_identical(
+    object = get_package_info(list(
+      base = c(), # nolint: unnecessary_concatenation_linter
+      attached = c(), # nolint: unnecessary_concatenation_linter
+      loaded = c() # nolint: unnecessary_concatenation_linter
+    )),
+    expected = list(
+      base = list(),
+      attached = list(),
+      loaded = list()
+    )
+  )
+})
 
-# test_that("get_package_info with NULL args returns empty lists", {
-#   expect_identical(
-#     object = get_package_info(list(
-#       base = NULL,
-#       attached = NULL,
-#       loaded = NULL
-#     )),
-#     expected = list(
-#       base = list(),
-#       attached = list(),
-#       loaded = list()
-#     )
-#   )
-# })
+test_that("get_package_info with NULL args returns empty lists", {
+  expect_identical(
+    object = get_package_info(list(
+      base = NULL,
+      attached = NULL,
+      loaded = NULL
+    )),
+    expected = list(
+      base = list(),
+      attached = list(),
+      loaded = list()
+    )
+  )
+})
 
-# test_that("get_package_info respects order", {
-#   digest_info <- get_individual_package_info("digest")
-#   utils_info <- get_individual_package_info("utils")
-#   expect_identical(
-#     object = get_package_info(
-#       list(
-#         base = c("digest", "utils"),
-#         attached = c("utils", "digest")
-#       )
-#     ),
-#     expected = list(
-#       base = list(
-#         digest = digest_info,
-#         utils = utils_info
-#       ),
-#       attached = list(
-#         utils = utils_info,
-#         digest = digest_info
-#       )
-#     )
-#   )
-# })
+test_that("get_package_info respects order", {
+  digest_info <- get_individual_package_info("digest")
+  utils_info <- get_individual_package_info("utils")
+  expect_identical(
+    object = get_package_info(
+      list(
+        base = c("digest", "utils"),
+        attached = c("utils", "digest")
+      )
+    ),
+    expected = list(
+      base = list(
+        digest = digest_info,
+        utils = utils_info
+      ),
+      attached = list(
+        utils = utils_info,
+        digest = digest_info
+      )
+    )
+  )
+})
