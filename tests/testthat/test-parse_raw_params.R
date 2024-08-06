6e60bd80e3dca2a4d46b2cf6b911e55e8375a1c5
Alex Axthelm <aaxthelm@RMI.org>
2024-08-06 15:49:23 +0200

    Add tests for raw schema validation

--- tests/testthat/test-parse_raw_params.R
+++ tests/testthat/test-parse_raw_params.R
@@ -90,7 +90,7 @@ schema_dir <- withr::local_tempdir()
 schema_file <- file.path(schema_dir, "product.json")
 writeLines(product_schema, schema_file)
 
-test_that("No inheritence, pass as string, schema validation works", {
+test_that("No inheritence, schema validation works", {
   json_string <- '{
     "id": 1,
     "name": "A green door",
@@ -122,8 +122,17 @@ writeLines(
   base_01_string,
   file.path(base_params_dir, "base01.json")
 )
+base_02_string <- '{
+    "name": "A blue door",
+    "tags": ["home", "blue"],
+    "supplier": "FooBar Doors"
+  }'
+writeLines(
+  base_02_string,
+  file.path(base_params_dir, "base02.json")
+)
 
-test_that("simple inheritence, pass as string, schema validation works", {
+test_that("simple inheritence, schema validation works", {
   json_string <- '{
     "id": 1,
     "price": 12.50,
@@ -143,5 +152,137 @@ test_that("simple inheritence, pass as string, schema validation works", {
       id = 1L,
       price = 12.5
     )
+  )
+})
+
+test_that("simple inheritence 2, schema validation works", {
+  json_string <- '{
+    "id": 1,
+    "price": 12.50,
+    "inherit": "base02"
+  }'
+  results <- parse_raw_params(
+    json = json_string,
+    schema_file = schema_file,
+    inheritence_search_paths = base_params_dir
+  )
+  expect_identical(
+    object = results,
+    expected = list(
+      name = "A blue door",
+      tags = c("home", "blue"),
+      supplier = "FooBar Doors",
+      id = 1L,
+      price = 12.5
+    )
+  )
+})
+
+raw_schema <- '{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "rawProduct",
+  "description": "Valid Input params for product",
+  "type": "object",
+  "properties": {
+    "inherit": {
+      "description": "Valid keys for inheritence",
+      "type": "string",
+      "enum": [
+        "base01",
+        "base02"
+      ]
+    }
+  },
+  "anyOf": [
+    {
+      "required": [
+        "inherit"
+      ]
+    },
+    {
+      "$ref": "product.json"
+    }
+  ]
+}'
+raw_schema_file <- file.path(schema_dir, "rawProduct.json")
+writeLines(raw_schema, raw_schema_file)
+
+test_that("No inheritence, raw validation works", {
+  json_string <- '{
+    "id": 1,
+    "name": "A green door",
+    "price": 12.50,
+    "tags": ["home", "green"]
+  }'
+  results <- parse_raw_params(
+    json = json_string,
+    schema_file = schema_file,
+    raw_schema_file = raw_schema_file
+  )
+  expect_identical(
+    object = results,
+    expected = list(
+      id = 1L,
+      name = "A green door",
+      price = 12.5,
+      tags = c("home", "green")
+    )
+  )
+})
+
+test_that("simple inheritence, raw validation works - inherit", {
+  json_string <- '{
+    "id": 1,
+    "price": 12.50,
+    "inherit": "base01"
+  }'
+  results <- parse_raw_params(
+    json = json_string,
+    schema_file = schema_file,
+    inheritence_search_paths = base_params_dir,
+    raw_schema_file = raw_schema_file
+  )
+  expect_identical(
+    object = results,
+    expected = list(
+      name = "A green door",
+      tags = c("home", "green"),
+      supplier = "ACME Doors",
+      id = 1L,
+      price = 12.5
+    )
+  )
+})
+
+test_that("raw validation errors correctly, no inherit field", {
+  json_string <- '{
+    "id": 1,
+    "price": 12.50
+  }'
+  expect_error(
+    parse_raw_params(
+      json = json_string,
+      schema_file = schema_file,
+      inheritence_search_paths = base_params_dir,
+      raw_schema_file = raw_schema_file
+    ),
+    "Invalid raw input parameters."
+  )
+})
+
+test_that("raw validation errors correctly, invalid inherit field", {
+  json_string <- '{
+    "id": 1,
+    "price": 12.50,
+    "inherit": "base03"
+  }'
+  expect_error(
+    parse_raw_params(
+      json = json_string,
+      schema_file = schema_file,
+      inheritence_search_paths = base_params_dir,
+      raw_schema_file = raw_schema_file
+    ),
+    "Invalid raw input parameters."
   )
 })
