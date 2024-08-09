a88a218da4534476d99730d47f86c8608365a7c4
Alex Axthelm <aaxthelm@RMI.org>
2024-08-09 10:59:49 +0200

    Update and render docs

--- R/file_checks.R
+++ R/file_checks.R
@@ -1,3 +1,13 @@
+#' check_dir_writable
+#'
+#' Check if a directory is writable. Check is performed by attempting to write
+#' a 0-byte file (`.test`) to the directory (and removing afterwards). Emits a
+#' warning on windows, since file permissions on Windows systems are difficult
+#' to test, but _should_ be accurate.
+#'
+#' @param dir directory to check writiblity
+#' @return logical, can a file be written to the directory?
+#' @export
 check_dir_writable <- function(dir) {
   log_trace("Checking if directory is writable: ", dir)
   if (tolower(Sys.info()["sysname"]) == "windows") {
