c65d0d92e642eb32231789195d6fd33b64f23099
Alex Axthelm <aaxthelm@RMI.org>
2024-08-26 12:02:11 +0200

    update tests

--- tests/testthat/test-modify_single_list_element.R
+++ tests/testthat/test-modify_single_list_element.R
@@ -1,8 +1,8 @@
-test_that("modify_list_element correctly modifies a flat list", {
+test_that("modify_single_list_element correctly modifies a flat list", {
   test_list <- list(a = 1L, b = 2.2, c = "a")
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    positions = "b",
+    position = "b",
     function_to_apply = I
   )
   expect_identical(
@@ -11,11 +11,11 @@ test_that("modify_list_element correctly modifies a flat list", {
   )
 })
 
-test_that("modify_list_element accepts different functions", {
+test_that("modify_single_list_element accepts different functions", {
   test_list <- list(a = 1L, b = 2.2, c = "a")
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    positions = "b",
+    position = "b",
     function_to_apply = as.integer
   )
   expect_identical(
@@ -24,11 +24,11 @@ test_that("modify_list_element accepts different functions", {
   )
 })
 
-test_that("modify_list_element correctly modifies a nested list", {
+test_that("modify_single_list_element correctly modifies a nested list", {
   test_list <- list(a = 1L, b = list(ba = 2L, bb = 2.2), c = "a")
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    positions = c("b", "bb"),
+    position = c("b", "bb"),
     function_to_apply = I
   )
   expect_identical(
@@ -37,27 +37,11 @@ test_that("modify_list_element correctly modifies a nested list", {
   )
 })
 
-test_that("modify_single_list_element modifies multiple elements", {
+test_that("modify_single_list_element modifies list elements of nested list", {
   test_list <- list(a = 1L, b = list(ba = 2L, bb = 2.2), c = "a")
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    position = list(
-      c("b", "bb"),
-      "a"
-    ),
-    function_to_apply = I
-  )
-  expect_identical(
-    object = results,
-    expected = list(a = I(1L), b = list(ba = 2L, bb = I(2.2)), c = "a")
-  )
-})
-
-test_that("modify_list_element modifies list elements of nested list", {
-  test_list <- list(a = 1L, b = list(ba = 2L, bb = 2.2), c = "a")
-  results <- modify_list_element(
-    x = test_list,
-    positions = "b",
+    position = "b",
     function_to_apply = I
   )
   expect_identical(
@@ -66,7 +50,7 @@ test_that("modify_list_element modifies list elements of nested list", {
   )
 })
 
-test_that("modify_list_element correctly modifies an deep nested list", {
+test_that("modify_single_list_element correctly modifies an deep nested list", {
   test_list <- list(
     a = 1L,
     b = list(
@@ -83,9 +67,9 @@ test_that("modify_list_element correctly modifies an deep nested list", {
       )
     )
   )
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    positions = c("b", "d", "f", "h", "i"),
+    position = c("b", "d", "f", "h", "i"),
     function_to_apply = I
   )
   expect_identical(
@@ -109,7 +93,7 @@ test_that("modify_list_element correctly modifies an deep nested list", {
   )
 })
 
-test_that("modify_list_element can accept positional arguments", {
+test_that("modify_single_list_element can accept positional arguments", {
   test_list <- list(
     a = 1L,
     b = list(
@@ -126,9 +110,9 @@ test_that("modify_list_element can accept positional arguments", {
       )
     )
   )
-  results <- modify_list_element(
+  results <- modify_single_list_element(
     x = test_list,
-    positions = c(2L, 2L, 2L, 2L, 1L),
+    position = c(2L, 2L, 2L, 2L, 1L),
     function_to_apply = I
   )
   expect_identical(
@@ -141,54 +125,6 @@ test_that("modify_list_element can accept positional arguments", {
           e = 3L,
           f = list(
             g = 4L,
-            h = list(
-              i = I(5L),
-              j = 6L
-            )
-          )
-        )
-      )
-    )
-  )
-})
-
-test_that("modify_list_element can modify multiple nested list elements", {
-  test_list <- list(
-    a = 1L,
-    b = list(
-      c = 2L,
-      d = list(
-        e = 3L,
-        f = list(
-          g = 4L,
-          h = list(
-            i = 5L,
-            j = 6L
-          )
-        )
-      )
-    )
-  )
-  results <- modify_list_element(
-    x = test_list,
-    positions = list(
-      c("b", "d", "f", "h", "i"),
-      "a",
-      c("b", "c"),
-      c("b", "d", "f", "g")
-    ),
-    function_to_apply = I
-  )
-  expect_identical(
-    object = results,
-    expected = list(
-      a = I(1L),
-      b = list(
-        c = I(2L),
-        d = list(
-          e = 3L,
-          f = list(
-            g = I(4L),
             h = list(
               i = I(5L),
               j = 6L
