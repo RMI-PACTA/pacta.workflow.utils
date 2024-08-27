83a3d99ad0d57ab740609f01995c139b5431f218
Alex Axthelm <aaxthelm@RMI.org>
2024-08-27 11:14:31 +0200

    Suppress logging in tests

--- tests/testthat/test-modify_single_list_element.R
+++ tests/testthat/test-modify_single_list_element.R
@@ -1,3 +1,18 @@
+## save current settings so that we can reset later
+threshold <- logger::log_threshold()
+appender  <- logger::log_appender()
+layout    <- logger::log_layout()
+on.exit({
+  ## reset logger settings
+  logger::log_threshold(threshold)
+  logger::log_layout(layout)
+  logger::log_appender(appender)
+})
+
+logger::log_appender(logger::appender_stdout)
+logger::log_threshold(logger::FATAL)
+logger::log_layout(logger::layout_simple)
+
 test_that("modify_single_list_element correctly modifies a flat list", {
   test_list <- list(a = 1L, b = 2.2, c = "a")
   results <- modify_single_list_element(
