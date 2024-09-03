f8d3978a28c094a1e5ba3b4d3fe52980487393e8
Alex Axthelm <aaxthelm@RMI.org>
2024-09-03 09:45:04 +0200

    Lint tests

--- tests/testthat/test-un_asis.R
+++ tests/testthat/test-un_asis.R
@@ -14,15 +14,14 @@ logger::log_threshold(logger::FATAL)
 logger::log_layout(logger::layout_simple)
 
 test_that("un_asis does not alter objects without AsIs class", {
-  x <- rnorm(10)
+  x <- rnorm(10L)
   results <- un_asis(x)
   expect_identical(results, x)
 })
 
 test_that("un_asis removes AsIs class", {
-  x <- rnorm(10)
+  x <- rnorm(10L)
   x_asis <- I(x)
   results <- un_asis(x_asis)
   expect_identical(results, x)
 })
-
