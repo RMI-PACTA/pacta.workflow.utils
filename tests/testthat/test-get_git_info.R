ce9cb57d9109ae757fea65e28ce5cd6d41fdcbd3
Alex Axthelm <aaxthelm@RMI.org>
2024-11-01 12:57:43 +0100

    Remove per-test logger setup/teardown

--- tests/testthat/test-get_git_info.R
+++ tests/testthat/test-get_git_info.R
@@ -1,18 +1,3 @@
-## save current settings so that we can reset later
-threshold <- logger::log_threshold()
-appender  <- logger::log_appender()
-layout    <- logger::log_layout()
-on.exit({
-  ## reset logger settings
-  logger::log_threshold(threshold)
-  logger::log_layout(layout)
-  logger::log_appender(appender)
-})
-
-logger::log_appender(logger::appender_stdout)
-logger::log_threshold(logger::FATAL)
-logger::log_layout(logger::layout_simple)
-
 # TESTS BEGIN
 test_that("get_git_info processes non-git-repo correctly", {
   test_dir <- withr::local_tempdir()
