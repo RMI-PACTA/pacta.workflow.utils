dcb1db1840df4ab78264c957369ba1c53019e71c
Alex Axthelm <aaxthelm@RMI.org>
2024-11-07 11:11:35 +0100

    remove logger setup/teardown for individual test files

--- tests/testthat/test-un_asis.R
+++ tests/testthat/test-un_asis.R
@@ -1,18 +1,3 @@
-## save current settings so that we can reset later
-threshold <- logger::log_threshold()
-appender  <- logger::log_appender()
-layout    <- logger::log_layout()
-on.exit({
-  ## reset logger settings
-  logger::log_threshold(threshold)
-  logger::log_layout(layout)
-  logger::log_appender(appender)
-})
-
-logger::log_appender(logger::appender_stdout)
-logger::log_threshold(logger::FATAL)
-logger::log_layout(logger::layout_simple)
-
 test_that("un_asis does not alter objects without AsIs class", {
   x <- rnorm(10L)
   results <- un_asis(x)
